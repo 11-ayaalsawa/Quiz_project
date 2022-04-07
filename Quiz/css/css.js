1e80bc8e46da4d5c1555e8207c524c92612d4a85
11-ayaalsawa <ayaalsawa279@gmail.com>
2022-04-07 23:25:29 +0300

    project

---
+++ Quiz/css/css.js
@@ -0,0 +1,197 @@
+"use strict";
+
+let cssquestions = [
+    {
+    numb: 1,
+    question1: "What does CSS stands for ?",
+    answer: "Cascading style sheets",
+    options: [
+      "Cascade style sheets",
+      "Color and style sheets",
+      "Cascading style sheets",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question1: " The HTML attribute used to define the inline styles is ?",
+    answer: "style",
+    options: [
+      "style",
+      "styles",
+      "class",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question1: "Which of the following CSS property is used to set the background image of an element?",
+    answer: "background-image",
+    options: [
+      "background-attachment",
+      "background-image",
+      "background-color",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question1: "Which of the following is the correct syntax to display the hyperlinks without any underline?",
+    answer: "a {text-decoration : none;}",
+    options: [
+      "a {text-decoration : underline;}",
+      "a {decoration : no-underline;}",
+      " a {text-decoration : none;}",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question1: " How to select the elements with the class name 'example'?",
+    answer: ".example",
+    options: [
+      "example",
+      "#example",
+      ".example",
+      "Class example"
+    ]
+  }];
+
+
+ 
+
+  var min = 120;
+    
+var time = setInterval(function() {
+  var minutes = parseInt((min % (60 * 60)) / (60));
+  var seconds = parseInt(min % (60));
+  document.getElementById("timer").innerHTML = minutes + ":" + seconds ;
+  if (min < 0) {
+    clearInterval(timer);
+    window.location="../HTML/show.html"
+    
+  }
+  min =  min - 1;
+  // console.log(min);
+}, 1000);
+
+
+
+
+////////////////////////////////////////////////
+// let x=[];
+// function showQue(x){
+
+
+//     que.innerHTML=x.question;
+// let label1=document.getElementById("label1").innerHTML=x.options[0];
+// let label2=document.getElementById("label2").innerHTML=x.options[1];
+// let label3=document.getElementById("label3").innerHTML=x.options[2];
+// let label4=document.getElementById("label4").innerHTML=x.options[3];
+
+// }
+
+let x=[];
+function startquiz(){
+    quebody( cssquestions[0] )
+  
+    
+  }
+  
+  function quebody( x ){
+    document.getElementById("queque").innerHTML= x.question1;
+    document.getElementById("label1").innerHTML= x. options[0];
+    document.getElementById("label2").innerHTML= x. options[1];
+    document.getElementById("label3").innerHTML= x. options[2];
+    document.getElementById("label4").innerHTML= x. options[3];
+  
+  }
+  /////////////////////////////////////////////////////////
+//   let counter=0;
+
+//   function click(x){
+//       if (x==cssquestions[i].answer){
+
+//         document.getElementById("msg").innerHTML="message";
+//         counter++;
+//         nextQue();
+//       }
+//   }
+
+//////////////////////////////////////////////////////////
+  startquiz()
+  let i=0;
+  function nextQue(){
+      if (i==cssquestions.length-1){
+          document.getElementById("next").innerHTML="Submit";
+        //   window.location=""
+      }
+
+      else{
+          i++;
+          quebody(cssquestions[i]);
+
+      }
+
+      
+
+  }
+
+  let counter=0;
+
+  function click(x){
+      if (x==cssquestions[i].answer){
+
+        document.getElementById("msg").innerHTML="message";
+        counter++;
+        nextQue();
+      }
+  }
+
+
+
+//////////////////////////////////////////////
+let useranswer=[];
+let correctanswer=[];
+
+let choose=0;
+function choose1(){
+    choose=1;
+
+}
+
+function choose2(){
+    choose=2;
+
+}
+
+function choose3(){
+    choose=3;
+
+}
+
+function choose4(){
+    choose=4;
+
+}
+
+
+
+ function next(){
+
+    quebody();
+    nextQue();
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
